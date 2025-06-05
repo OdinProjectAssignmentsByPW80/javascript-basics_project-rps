fb224b0892ce7fa3f3132989405221ef8701f9bc
Paul Williams <advanced_solution_delivery@outlook.com>
2025-06-05 10:01:43 +0100

    feat playRound
    refactor getComputerChoice() and gerPlayerChoice() to return int
    for easier comparison
    
    Changes to be committed:
    	modified:   rps-script.js

--- rps-script.js
+++ rps-script.js
@@ -6,20 +6,39 @@ const choices = ["ROCK", "PAPER", "SCISSORS"];
 
 /**
  * Generate a random choice for the computer.
- * @returns {string} One of the three choices: "ROCK", "PAPER" or "SCISSORS".
+ * @returns {int} Refers to an index in choices.
  */
 function getComputerChoice() {
-  return choices[Math.floor(Math.random() * 3)];
+  return Math.floor(Math.random() * 3);
 }
 
 /**
  * Prompts player to make a choice.
- * @returns {string} One of the three choices: "ROCK", "PAPER" or "SCISSORS".
+ * @returns {int} Refers to index in choices.
  */
 function getPlayerChoice() {
   const promptText = "Please enter a number..\n1: ROCK\n2: PAPER\n3: SCISSORS";
-  // return prompt(promptText, 0);
-  return choices[prompt(promptText) - 1];
+  return prompt(promptText) - 1;
+}
+
+/**
+ * Plays a single round of paper rock scissors between the computer and player.
+ * @returns {Array<int, int, string>} Contains the computers choice as an int,
+ * the players choice as an int and a string describing the result of the round.
+ */
+function playRound() {
+  const computerChoice = getComputerChoice();
+  const playerChoice = getPlayerChoice();
+  if (playerChoice == computerChoice) {
+    return [computerChoice, playerChoice, "Draw"];
+  }
+  let playerWon = playerChoice > computerChoice;
+  if (playerChoice == 0 && computerChoice == 2) playerWon = !playerWon;
+  return [
+    computerChoice,
+    playerChoice,
+    playerWon ? "Player Won" : "Player Lost",
+  ];
 }
 
 /* #endregion */
@@ -31,7 +50,10 @@ function getPlayerChoice() {
 //   console.log(getComputerChoice());
 // }
 
-/* getPlayer Choice */
+/* getPlayerChoice */
 // console.log("player", getPlayerChoice());
+
+/* playRound */
+// console.log(playRound());
 
 /* #endregion */
