de03118956b8b2eb77997c60532c83ec999b8e62
Paul Williams <advanced_solution_delivery@outlook.com>
2025-06-05 09:09:02 +0100

    feat: getComputerChoice
    Changes to be committed:
    	modified:   rps-script.js

--- rps-script.js
+++ rps-script.js
@@ -1,3 +1,23 @@
 "use strict";
 
-console.log("Script linked and working!!");
+/* #region  MAIN */
+
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+
+/**
+ * Generate a random choice for the computer.
+ * @returns {string} One of the three choices: "ROCK", "PAPER" or "SCISSORS".
+ */
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * 3)];
+}
+
+/* #endregion */
+
+/* #region  TESTING */
+
+for (let i = 0; i < 10; i++) {
+  console.log(getComputerChoice());
+}
+
+/* #endregion */
